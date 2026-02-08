abc23b596e839397171a05f368304962cac9d1b1
Guttula Sai Priya <sattibabu.guttula88@gmail.com>
2026-02-08 07:26:14 +0000

    this is the last final error .. fix these lines .. Application error: a

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -131,7 +131,7 @@ const SidebarProvider = React.forwardRef<
                 "--sidebar-width": SIDEBAR_WIDTH,
                 "--sidebar-width-icon": SIDEBAR_WIDTH_ICON,
                 ...style,
-              } as React.Properties
+              } as React.CSSProperties
             }
             className={cn(
               "group/sidebar-wrapper flex min-h-svh w-full has-[[data-variant=inset]]:bg-sidebar",
